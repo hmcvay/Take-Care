7ebc165bb3279fd29867c1170211d759598ec4a6
hmcvay <hayleymcvay8@gmail.com>
2022-05-17 09:15:26 -0700

    Add async method for pulling data collection from firebase. And useEffect hook

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,41 +2,94 @@ import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
+  Image,
   StyleSheet,
   TouchableOpacity,
   SafeAreaView,
+  FlatList,
 } from 'react-native';
+import { useFocusEffect } from "@react-navigation/native";
 import { db } from './../firebase/firebase-config';
-import { collection, getDocs, doc, setDoc, Timestamp } from 'firebase/firestore';
+import { collection, getDocs, doc, addDoc, Timestamp } from 'firebase/firestore';
 import Icon from 'react-native-vector-icons/dist/SimpleLineIcons';
-import { windowWidth } from "../utilities/Dimensions";
+import { windowHeight, windowWidth } from "../utilities/Dimensions";
 import { Dimensions } from 'react-native';
 
 
 function PostList({navigation, route}){
 
-  const width = Dimensions.get('window').width;
+  const [postList, setPostList] = useState({empty: true});
+  const [update, setUpdate] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const {params} = route;
 
-  // const [postList, setPostList] = useState({empty: true});
-  // const [update, setUpdate] = useState(false);
-  // const [isLoading, setIsLoading] = useState(false);
-  // const {params} = route;
+  const messageDisplay = () => {
+    if (params) {
+      const now = new Date().getTime();
+      if (params.message && now - params.messageTimeOut < 1000) {
+        return (
+          <View>
+            <Text style={{fontWeight: 'bold', fontSize: 18}}>
+              {params.message}
+            </Text>
+          </View>
+        );
+      }
+    }
+  };
 
-  // const getList = async () => {
-  //   setIsLoading(true);
-  //   const postCollection = collection(db, 'posts');
-  //   const postSnapshot = await getDocs(postsCollection);
-  //   const newPostList = postSnapshot.docs.map(p => p.data());
-  //   newPostList.map(post => {
-  //     post.time; 
-  //   });
-  //   setPostList(newPostList);
-  //   setIsLoading(false);
-  // };
+  const getList = async () => {
+    setIsLoading(true);
+    const postsCollection = collection(db, 'posts');
+    const postSnapshot = await getDocs(postsCollection);
+    const newPostList = postSnapshot.docs.map(p => p.data());
+    newPostList.map(post => {
+      post.time; 
+    });
+    setPostList(newPostList);
+    setIsLoading(false);
+    const date = new Date();
+  };
+
+  const checkIfLoading = () => {
+    if (isLoading === true || postList.empty === true){
+      return (
+        <View>
+          <Image 
+            source={require('./../assets/takeCareTransparent.png')}
+            opacity={0.5}
+            style={{width: 200, height: 200}}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={{height: windowHeight - 200}}>
+          {messageDisplay()}
+          <FlatList 
+            data={postList}
+            renderItem={({post}) => <>{postDisplayBlock(post)}</>}
+          />
+        </View>
+      )
+    }
+  }
+
+  useEffect(() => {
+    const now = new Date().getTime();
+  }, [isLoading]);
+
+  useFocusEffect(() => {
+    () => getList();
+  }, [update]);
+
+  useEffect(() => {
+    getList();
+  }, [update]);
 
   return (
     <View style={styles.pageLayout}>
-      {/* {checkIfLoading()} */}
+      {checkIfLoading()}
       <View>
         <TouchableOpacity>
           <View style={styles.buttons}>
