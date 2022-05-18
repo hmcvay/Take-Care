d6e2a247960ce2841499f5f28a051bba93a8c8fa
hmcvay <hayleymcvay8@gmail.com>
2022-05-17 21:58:32 -0700

    Add hard code posts to post list while firebase is not working...

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -13,178 +13,124 @@ import { db } from './../firebase/firebase-config';
 import { collection, getDocs, doc, Timestamp, DocumentData } from 'firebase/firestore';
 import Icon from 'react-native-vector-icons/dist/SimpleLineIcons';
 import { windowHeight, windowWidth } from "../utilities/Dimensions";
+import { posts } from './../seed/data';
+import { fonts } from "react-native-elements/dist/config";
 
+function PostList({navigation}){
 
-function PostList({navigation, route}){
-
-  const [postList, setPostList] = useState({empty: true});
-  const [update, setUpdate] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function getData(){
-    setIsLoading(true);
-    // let array = [];
-    const postCollection = collection(db, "posts");
-    const postSnapshot = await getDocs(postCollection);
-    const newPostList = postSnapshot.docs.map(d => d.data());
-
-    // postSnapshot.forEach((doc) => {
-    //   array.push(doc.data());
-    //   console.log(doc.data());
-    // });
-    // setPostList(newPostList);
-    setPostList(newPostList);
-    setIsLoading(false);
-    // console.log('ARRAYYYYY!!!!!' + array);
-  };
-
-  useEffect(() => {
-    getData();
-  }, [update]);
-  
-  // getData();
- 
-  
   // const [postList, setPostList] = useState({empty: true});
   // const [update, setUpdate] = useState(false);
   // const [isLoading, setIsLoading] = useState(false);
-  // const {params} = route;
 
-  // const messageDisplay = () => {
-  //   if (params) {
-  //     const now = new Date().getTime();
-  //     if (params.message && now - params.messageTimeOut < 1000) {
-  //       return (
-  //         <View>
-  //           <Text style={{fontWeight: 'bold', fontSize: 18}}>
-  //             {params.message}
-  //           </Text>
-  //         </View>
-  //       );
-  //     }
-  //   }
-  // };
-
-  // const getList = async () => {
+  // async function getData(){
   //   setIsLoading(true);
-  //   const postsCollection = collection(db, 'posts');
-  //   const postsSnapshot = await getDocs(postsCollection);
-  //   const newPostList = Object.entries(postsSnapshot);
-  //   // newPostList.map(post => {
-  //   //   post.title
-  //   // });
+  //   const postCollection = collection(db, 'posts');
+  //   const postSnapshot = await getDocs(postCollection);
+  //   const newPostList = postSnapshot.docs.map(d => d.data());
   //   setPostList(newPostList);
-  //   console.log('NEW POSTLIST = ' + newPostList);
   //   setIsLoading(false);
   // };
 
   // useEffect(() => {
-  //   getList();
+  //   getData();
   // }, [update]);
 
   // useFocusEffect(() => {
   //   () => getList();
   // });
 
-  useEffect(() => {
-    const now = new Date().getTime();
-    console.log('isLoading = ' + ' ' + now);
-  }, [isLoading]);
+  // useEffect(() => {
+  // }, [isLoading]);
 
-  const checkIfLoading = () => {
-    if (isLoading === true || postList.empty === true){
-      return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-          <Image 
-            source={require('./../assets/takeCareTransparent.png')}
-            opacity={0.5}
-            style={{width: 200, height: 200}}
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View style={{height: windowHeight - 200}}>
-          {/* {messageDisplay()} */}
-          <FlatList 
-            data={postList}
-            renderItem={({post}) => <>{postDisplayBlock(post)}</>}
-          />
-        </View>
-      );
-    }
-  };
+  // const checkIfLoading = () => {
+  //   if (isLoading === true || postList.empty === true){
+  //     return (
+  //       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+  //         <Image 
+  //           source={require('./../assets/takeCareTransparent.png')}
+  //           opacity={0.5}
+  //           style={{width: 200, height: 200}}
+  //         />
+  //       </View>
+  //     );
+  //   } else {
+  //     return (
+  //       <View style={{height: windowHeight - 200}}>
+  //         <FlatList 
+  //           data={postList}
+  //           renderItem={({post}) => <>{postDisplayBlock(post)}</>}
+  //         />
+  //       </View>
+  //     );
+  //   }
+  // };
 
-  const postDisplayBlock = post => {
-    console.log("THESE ARE POSTS!!!!" + {post})
-    return (
-      <TouchableOpacity onPress={() => navigation.navigate('PostDetail', {post})}
-        style = {styles.post}>
-        <View style={styles.postDisplay}>
-          <View style={styles.postBody}>
-            <Text style={styles.postTitle}>{post.title}</Text>
-            <Text style={styles.postLocation}>{post.location}</Text>
-            <Text style={styles.postDescription} numberOfLines={5}>{post.description}</Text>
-          </View>
-          <View>
-            <Text style={styles.postUrgent}>
-              {post.is_urgent ? `this post is urgent!` : ``}
-            </Text>
-          </View>
-        </View>
-      </TouchableOpacity>
-    );
-  };
+  // const postDisplayBlock = post => {
+  //   console.log("THESE ARE POSTS!!!!" + {post})
+  //   return (
+  //     <TouchableOpacity onPress={() => navigation.navigate('PostDetail', {post})}
+  //       style = {styles.post}>
+  //       <View style={styles.postDisplay}>
+  //         <View style={styles.postBody}>
+  //           <Text style={styles.postTitle}>{post.title}</Text>
+  //           <Text style={styles.postLocation}>{post.location}</Text>
+  //           <Text style={styles.postDescription} numberOfLines={5}>{post.description}</Text>
+  //         </View>
+  //         <View>
+  //           <Text style={styles.postUrgent}>
+  //             {post.is_urgent ? `this post is urgent!` : ``}
+  //           </Text>
+  //         </View>
+  //       </View>
+  //     </TouchableOpacity>
+  //   );
+  // };
 
   return (
     <View style={styles.pageLayout}>
-      {checkIfLoading()}
+     {/* {checkIfLoading()} */}
+      <View style={styles.post}>
+        <Text style={styles.postTitle}>NEED BOOTS</Text>
+        <Text style={styles.postLocation}>sullivan's gulch: holladay and 25th</Text>
+        <Text style={styles.postDescription}>Mutual PDX is collecting as many pairs of durable, warm, new, or gently used boots as possible for our unhoused neighbors! We will be out collecting donations until late evening May 15, down on the stretch of Holladay between 23rd and 28th</Text>
+        <View style={styles.postButtons}>
+          <Icon
+            style={{fontSize: 25, paddingRight: 20 }}
+            name="paper-clip"
+          />
+          <Icon
+            style={{fontSize: 25}} 
+            name="heart"
+          />
+        </View>
+      </View>
       <View>
         <TouchableOpacity>
           <View style={styles.buttons}>
             <Icon 
               style={{color: '#ed4b2a', fontSize: 30, bottom: 2}}
-              name="reload"
-              onPress={() => setUpdate(prevState => !prevState)}
+              name="list"
+              onPress={() => navigation.navigate('PostList')}
             />
             <Icon
               style={{color: '#ed4b2a', fontSize:30, bottom: 2}}
-              name="speech"
+              name="pencil"
               onPress={() => navigation.navigate('AddPost')}
+            />
+            <Icon
+              style={{color: '#ed4b2a', fontSize:30, bottom: 2}}
+              name="paper-clip"
             />
             <Icon 
               style={{color: '#ed4b2a', fontSize: 30, bottom: 2}}
-              name="user"
+              name="emotsmile"
               onPress={() => navigation.navigate('Home')}
             />
           </View>
         </TouchableOpacity>
       </View>
     </View>
-   
   )
-
-    // -------------------------
-
-
-  // const [postList, setPostList] = useState([]);
-  // const postCollectionRef = collection(db, 'posts');
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     const data = await getDocs(postCollectionRef);
-  //     setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-  //   };
-
-  //   getData();
-  //   console.log('POST LIST HERRRRRRRE' + postList)
-  // }, []);
-
-  // const postDisplayed = post => {
-  //   return (
-  //     <Text>{post}</Text>
-  //   )
-  // }
 };
 
 export default PostList;
@@ -202,12 +148,50 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginBottom: 40,
+    marginBottom: 15,
     marginLeft: 40,
     marginRight: 40,
-    borderTopColor: 'black',
-    borderTopWidth: 1,
     height: 80,
     
   },
+
+  post:{
+    flex: 2,
+    height: 300,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+
+  postTitle:{
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    paddingTop: 15,
+  },
+
+  postLocation: {
+    textAlign: 'center',
+    fontSize: 14,
+    marginBottom: 10,
+    marginTop:10,
+  },
+
+  postDescription:{
+    textAlign: 'center',
+    fontSize: 19,
+    marginTop: 10,
+    marginBottom: 15,
+    marginLeft: 12,
+    marginRight: 12,
+  },
+
+  postButtons: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginLeft: 25,
+    marginTop: 10,
+  }
+
 });
